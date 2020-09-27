b8a83409989780ed380d3e1df604d4a72952bdfd
María Rosa Sánchez <>
2020-09-27 18:05:17 -0500

    Add Favorites to Local Storage

--- src/components/VideoDetail/VideoDetail.component.jsx
+++ src/components/VideoDetail/VideoDetail.component.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
+import { useAuth } from "../../providers/Auth";
+import { FAVORITES_KEY } from "../../utils/constants";
+import { storage } from "../../utils/storage";
+
 const VideoDetail = ({ video }) => {
+  const { authenticated } = useAuth();
+
   if (!video) {
     return <div>Loading ...</div>;
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  const onAddFavorites = () => {
+    const lastFavorites = storage.get(FAVORITES_KEY) || "[]";
+    const favorites = JSON.parse(lastFavorites);
+    favorites.push(video);
+    const newFavorites = favorites.filter(
+      (favorite, index, self) =>
+        index === self.findIndex((f) => f.id.videoId === favorite.id.videoId)
+    );
+    storage.set(FAVORITES_KEY, JSON.stringify(newFavorites));
+  };
 
   return (
     <div className="component__video-detail">
@@ -18,8 +35,17 @@ const VideoDetail = ({ video }) => {
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       />
       <div className="video-detail-summary">
-        <h4>{video.snippet.title}</h4>
-        <p>{video.snippet.description}</p>
+        <div className="content">
+          <h4>{video.snippet.title}</h4>
+          <p>{video.snippet.description}</p>
+        </div>
+        <div className="favorites">
+          {authenticated && (
+            <button type="submit" onClick={onAddFavorites}>
+              Add to favorites
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
