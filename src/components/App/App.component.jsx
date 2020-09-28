3d083f6659a2c30387827c6908566bea004f9c88
María Rosa Sánchez <>
2020-09-27 22:38:38 -0500

    Remove useLayoutEffect

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import AuthProvider from "../../providers/Auth";
@@ -10,26 +10,8 @@ import Favorites from "../../pages/Favorites";
 import Private from "../Private";
 import Fortune from "../Fortune";
 import Layout from "../Layout";
-import { random } from "../../utils/fns";
 
 const App = () => {
-  useLayoutEffect(() => {
-    const { body } = document;
-
-    function rotateBackground() {
-      const xPercent = random(100);
-      const yPercent = random(100);
-      body.style.setProperty("--bg-position", `${xPercent}% ${yPercent}%`);
-    }
-
-    const intervalId = setInterval(rotateBackground, 3000);
-    body.addEventListener("click", rotateBackground);
-
-    return () => {
-      clearInterval(intervalId);
-      body.removeEventListener("click", rotateBackground);
-    };
-  }, []);
   const [activeVideo, setActiveVideo] = useState(null);
 
   return (
