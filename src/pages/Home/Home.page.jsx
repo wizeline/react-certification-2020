13abaf2b283a37958ef74d12554dd9127493d58c
Estefi Jacobo <estefania.jacobo@wizeline.com>
2020-09-29 10:47:12 -0500

    fix: remove mock data to avoid excedding api calls when testing

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -5,15 +5,14 @@ import React, { useState, useEffect } from 'react';
 import './Home.styles.css';
 import VideoCardPanel from '../../components/VideoCardPanel';
 
-function HomePage() {
+function HomePage({ inputState }) {
   // eslint-disable-next-line global-require
-  const json = require('../../utils/mockData.json');
   // const history = useHistory();
   // const { authenticated, logout } = useAuth();
   const [videos, setVideos] = useState([]);
 
-  // const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
-  // const API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${inputState}&key=${API_KEY}`;
+  const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const API_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${inputState}&key=${API_KEY}`;
 
   // function deAuthenticate(event) {
   //   event.preventDefault();
@@ -22,24 +21,18 @@ function HomePage() {
   // }
 
   useEffect(() => {
-    setVideos(json.items);
-  }, [json]);
-
-  // setVideos(json);
-  // useEffect(() => {
-  //   const fetchVideos = async () => {
-  //     const videosFetched = await fetch(API_URL).then((response) => response.json());
-  //     setVideos(videosFetched.items);
-  //   };
-  //   fetchVideos();
-  //   // return () => {
-  //   //   setVideos([]);
-  //   // };
-  // }, [inputState]);
+    const fetchVideos = async () => {
+      const videosFetched = await fetch(API_URL).then((response) => response.json());
+      setVideos(videosFetched.items);
+    };
+    fetchVideos();
+    return () => {
+      setVideos([]);
+    };
+  }, [inputState, API_URL]);
 
   return (
     <section className="homepage-wrapper">
-      {/* <Navbar inputState={inputState} setInputState={setInputState} /> */}
       {videos && <VideoCardPanel videos={videos} />}
     </section>
   );
