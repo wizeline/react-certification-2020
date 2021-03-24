65912d139e87249227b4f318a82b91ee4bdff552
Duarte <yo.956c@gmail.com>
2021-03-24 14:37:32 -0700

    Update src/components/CardList/CardList.jsx
    Co-authored-by: PacoMojica <pacomojica@hotmail.com>

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -6,9 +6,9 @@ const CardList = () => {
     const [data, setData] = useState();
 
     const fetchData = async () => {
-        const res = await fetch("https://gist.githubusercontent.com/jparciga/1d4dd34fb06ba74237f8966e2e777ff5/raw/f3af25f1505deb67e2cc9ee625a633f24d8983ff/youtube-videos-mock.json");
-        const result = await res.json();
-        setData(result);
+        const res = await fetch("https://gist.githubusercontent.com/jparciga/1d4dd34fb06ba74237f8966e2e777ff5/raw/f3af25f1505deb67e2cc9ee625a633f24d8983ff/youtube-videos-mock.json")
+        const result = await res.json()
+        setData(result)
     }
 
     useEffect(() => {
