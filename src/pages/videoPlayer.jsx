afeef176344ecb5f3ef5bce45ee97bd5bcbeb518
= <=>
2021-05-03 15:46:53 -0600

    let me see

--- src/pages/videoPlayer.jsx
+++ src/pages/videoPlayer.jsx
@@ -12,7 +12,7 @@ const videoPlayer = () => {
         <Flex>
             <Video/>
             <Box w="sm">
-                {data.player ? <CardList list={true} IdVid={data.player.id.videoId} /> : null}
+                {data.player.id ? <CardList list={true} IdVid={data.player.id.videoId} /> : null}
             </Box>
         </Flex>
             
