2b110c14e03d3f4ff49de9446264466006d59cf2
= <=>
2021-05-03 14:58:51 -0600

    yolo1

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 module.exports = {
-    webpack: (config, { isServer }) => {
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.node = {
-                fs: 'empty',
-                module: 'empty',
-                worker_threads: 'empty',
-                inspector: 'empty',
-                net: 'empty',
-                child_process: 'empty'
-            }
-        }
-
-        return config
-    },
     env: {
         API_KEY: 'AIzaSyBLF4iEDjnWFmKrhEtIetO5qRT2aKQqO4o',
     },
