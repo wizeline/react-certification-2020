453d2385685e920eedfb5a0fbd9e3e0ae66145c1
= <=>
2021-05-03 11:03:52 -0600

    adding next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     env: {
-      API_KEY: 'AIzaSyBLF4iEDjnWFmKrhEtIetO5qRT2aKQqO4o',
+        API_KEY: 'AIzaSyBLF4iEDjnWFmKrhEtIetO5qRT2aKQqO4o',
     },
-  }
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            config.node = {
+                fs: 'empty'
+            }
+        }
+
+        return config
+    }
+}
