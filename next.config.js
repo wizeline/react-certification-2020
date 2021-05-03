76a759fef7e2b2ffb65af300431afca24c91172e
= <=>
2021-05-03 14:19:17 -0600

    last dance

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            config.node = {
+                fs: 'empty',
+                module: 'empty'
+            }
+        }
+
+        return config
+    },
     env: {
         API_KEY: 'AIzaSyBLF4iEDjnWFmKrhEtIetO5qRT2aKQqO4o',
     },
